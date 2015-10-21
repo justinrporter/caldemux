84e1e2a0347a9a2565494147c74a268f237d8a61
Justin R. Porter <justinrporter@gmail.com>
2015-10-21 13:53:45 -0500

    Update index page, add warning for use of commas.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,15 @@
 angular.module('myApp', ['ui.bootstrap'])
-  .controller('ExampleController', ['$scope', function($scope) {
+  .controller('UrlBuilderController', ['$scope', function($scope) {
 
     $scope.produce_url = function(canvas_url, tag){
       if(canvas_url && tag){
+        if(tag.indexOf(',') != -1 ){
+          return "Whoa! Something doesn't look quite right with your tag! (Commas aren't allowed... are you trying to put in more than one tag?)"
+        }
+
         var canvas_uuid = canvas_url.slice(canvas_url.lastIndexOf('/')+1, canvas_url.indexOf('.ics'))
         var processed_tag = tag.replace(/ /g, '').replace('[', '').replace(']', '')
         return "https://wustl-caldemux.herokuapp.com/"+processed_tag+'/'+canvas_uuid
       }
   }
-
-    // $scope.canvas_uuid = url.slice(url.lastIndexOf('/'),url.indexOf('.ics'))
-
   }]);
