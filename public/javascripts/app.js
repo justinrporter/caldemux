7d2263c39cf89814f0b774289049219aec322332
Justin R. Porter <justinrporter@gmail.com>
2015-10-25 13:27:17 -0500

    Add an error for using the javascript calendar, and use URL validation built in to canvas properly.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,14 +2,16 @@ angular.module('myApp', ['ui.bootstrap'])
   .controller('UrlBuilderController', ['$scope', function($scope) {
 
     $scope.produce_url = function(canvas_url, tag){
-      if(canvas_url && tag){
-        if(tag.indexOf(',') != -1 ){
+      if(canvas_url && (canvas_url.indexOf('canvas.wustl.edu/calendar') != -1) ){
+        return "Whoa! Something doesn't look quite right with your url! Make sure you're giving us your Canvas feed URL, and not the Canvas calendar webapp URL."
+      } else if(tag && tag.indexOf(',') != -1 ){
           return "Whoa! Something doesn't look quite right with your tag! (Commas aren't allowed... are you trying to put in more than one tag?)"
-        }
+      }
 
+      if(canvas_url && tag){
         var canvas_uuid = canvas_url.slice(canvas_url.lastIndexOf('/')+1, canvas_url.indexOf('.ics'))
         var processed_tag = tag.replace(/ /g, '').replace('[', '').replace(']', '')
         return "https://wustl-caldemux.herokuapp.com/"+processed_tag+'/'+canvas_uuid
       }
-  }
+    }
   }]);
